b0906deee6fd557c9a3e7cff6bd0851d8c17266c
Joao <joao.g.leal@gmail.com>
2021-03-05 05:56:22 -0300

    logs show current time

--- __main__.py
+++ __main__.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     current_time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-    print(f'{current_time} Início da operação')
+    print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Início da operação')
     BASE_URL = 'https://www.amazon.com.br/PlayStation-Console-PlayStation%C2%AE5/dp/B088GNRX3J/ref\
         =sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=ps5&qid=1613470308&sr=8-1'
     # BASE_URL = 'https://www.amazon.com.br/God-War-Hits-PlayStation-4/dp/B07YT1GLV9/?_encoding=\
@@ -36,15 +36,18 @@ if __name__ == '__main__':
     chrome_options.add_argument("--no-sandbox")  # linux only
 
     if args.raspi:
-        print(f'{current_time} Operação headless em raspberry pi')
+        print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Operação headless em raspberry pi')
         chrome_options.add_argument("--headless")
         driver = webdriver.Chrome(options=chrome_options)
     else:
         driver = webdriver.Chrome(
             executable_path=DRIVER_PATH, options=chrome_options)
+
     driver.maximize_window()
     driver.implicitly_wait(10)
+    print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Busca URL')
     driver.get(BASE_URL)
+    print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} URL retornada')
 
     product.name = product.get_product_feature(
         driver, 'span', 'id', 'productTitle')
@@ -52,19 +55,21 @@ if __name__ == '__main__':
         driver, 'span', 'id', 'priceblock_ourprice')
 
     if not product.name:
-        print(f'{current_time} Nome do produto não encontrado')
+        print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Nome do produto não encontrado')
         # telegram_send.send(
         #     messages=["Ocorreu um erro ao buscar o produto, verificar no site"])
+    else:
+        print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Nome do produto encontrado com sucesso')
 
     if product.price:
         if product.name:
-            print(f'{current_time} Produto encontrado, enviando mensagem de alerta')
+            print(f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Produto encontrado, enviando mensagem de alerta')
             telegram_send.send(messages=[
                 f"Produto {product.name} encontrado por {product.price} na \
                 url abaixo: \n\n {product.url}"])
         else:
             print(
-                f'{current_time} Valor de produto encontrado, erro na busca pelo nome')
+                f'{datetime.now().strftime(' % d/%m/%Y % H: % M: % S')} Valor de produto encontrado, erro na busca pelo nome')
             telegram_send.send(messages=[
                 f"Produto encontrado por {product.price} na url abaixo: \n\n {product.url}"])
 
